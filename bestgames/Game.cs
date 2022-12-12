2bde65de1cee94a96d27a5a595fc4ba809371053
szilagyi.gergely <szilagyi.gergely@28-09>
2022-12-12 09:29:41 +0100

    Add project files.

---
+++ bestgames/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bestgames
+{
+    internal class Game
+    {
+        public int Date { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public List<string> Platforms { get; set; } = new();
+
+        public Game(string row)
+        {
+            string[] v = row.Split(';');
+            Date = int.Parse(v[0]);
+            Name = v[1];
+            Genre = v[2];
+            Publisher = v[3];
+            Platforms = v[4].Replace(", ", ",").Split(",").ToList();
+        }
+    }
+}
---
+++ bestgames/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace bestgames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader SR = new StreamReader("bestgames.csv");
+
+            List<Game> Games = new List<Game>();
+
+            string line;
+
+            while ((line = SR.ReadLine()) != null)
+            {
+                Games.Add(new Game(line));
+            }
+            SR.Close();
+
+            /*
+            foreach (var Game in Games)
+            {
+                Console.WriteLine(Game.Name);
+            }
+            */
+
+            //F1
+            Console.WriteLine($"f1: összesen {Games.Count()} játék szerepel a listában!");
+
+            //F2
+            var grp = Games.GroupBy(g => g.Date)
+                .Where(g => g.Count() > 10)
+                .OrderByDescending(g => g.Count())
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine($"f2: ezekben az években került ki több, mint 10 cím listára");
+            foreach (var kvp in grp)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}db");
+            }
+        }
+    }
+}
